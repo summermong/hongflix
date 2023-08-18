2dc638b7cc8c675e8e0bd1466a5281e408a3e160
summermong <2514o_o@naver.com>
2023-08-18 14:06:22 +0900

    :lipstick: 헤더 전역 설정

--- frontend/src/home/Header.js
+++ frontend/src/home/Header.js
@@ -16,19 +16,65 @@ const Header = () => {
     setDropdownVisible(false);
   };
 
+  // 스크롤 시 헤더 배경색 변경
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 5) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    // 스크롤 이벤트 리스너 추가
+    window.addEventListener('scroll', handleScroll);
+
+    // 컴포넌트 언마운트 시 스크롤 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="bg-indigo-950 p-4">
+    <header
+      className={`p-4 ${
+        isScrolled ? 'bg-indigo-950' : 'bg-transparent border-bottom'
+      }`}
+      style={{
+        position: 'sticky',
+        top: 0,
+        zIndex: 1000,
+        borderBottom: isScrolled ? 'none' : '1px solid black',
+        transition: 'background-color 0.3s ease-in',
+      }}
+    >
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex space-x-6 items-center">
-          <Link to="/" className="text-white text-xl font-semibold">
+          <Link
+            to="/"
+            className={`text-xl font-semibold ${
+              isScrolled ? 'text-white' : 'text-indigo-950'
+            }`}
+          >
             Hongflix
           </Link>
           <div className="relative">
-            <button onClick={toggleDropdown} className="text-white text-sm">
+            <button
+              onClick={toggleDropdown}
+              className={`text-sm ${
+                isScrolled ? 'text-white' : 'text-indigo-950'
+              }`}
+            >
               카테고리
             </button>
             {dropdownVisible && (
-              <div className="flex justify-center absolute top-8 right-0 -left-4 bg-gray-50 py-2 w-20 shadow-lg">
+              <div
+                className={`flex justify-center absolute top-8 right-0 -left-4 py-2 w-20 shadow-lg text-indigo-950 bg-white ${
+                  isScrolled ? 'null' : 'border-gray-950'
+                }`}
+              >
                 <ul className="text-sm text-center justify-center px-2">
                   <li className="py-0.5 px-1">
                     <Link to="/romance" onClick={closeDropdown}>
@@ -49,20 +95,35 @@ const Header = () => {
               </div>
             )}
           </div>
-          <Link to="/search" className="text-white text-sm">
+          <Link
+            to="/search"
+            className={`text-sm ${
+              isScrolled ? 'text-white' : 'text-indigo-950'
+            }`}
+          >
             검색
           </Link>
         </div>
         <nav className="space-x-4">
           {isLoggedIn ? (
             <>
-              <Link to="/mypage" className="text-white text-sm">
+              <Link
+                to="/mypage"
+                className={`text-sm ${
+                  isScrolled ? 'text-white' : 'text-indigo-950'
+                }`}
+              >
                 마이 페이지
               </Link>
             </>
           ) : (
             <>
-              <Link to="/login" className="text-white text-sm">
+              <Link
+                to="/login"
+                className={`text-sm ${
+                  isScrolled ? 'text-white' : 'text-indigo-950'
+                }`}
+              >
                 로그인
               </Link>
             </>
--- frontend/src/home/Main.js
+++ frontend/src/home/Main.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <div>dropdownVisible</div>
+    </div>
+  );
 };
 
 export default Main;
