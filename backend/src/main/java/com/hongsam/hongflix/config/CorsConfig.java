d59d5c5a0193357df553a8cb0fa1fc4e142ea152
dbstn1325 <dbstn1325@naver.com>
2023-08-24 21:03:30 +0900

    cors 수정g

--- backend/src/main/java/com/hongsam/hongflix/config/CorsConfig.java
+++ backend/src/main/java/com/hongsam/hongflix/config/CorsConfig.java
@@ -11,10 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청에 대해 CORS 설정 적용
-                .allowedOrigins("https://f8cf-61-254-190-217.ngrok-free.app", "https://a409-218-154-176-12.ngrok-free.app") // 허용할 도메인 지정
+                .allowedOrigins("*") // 허용할 도메인 지정
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS") // 허용할 HTTP 메서드 지정
-                .allowedHeaders("*") // 허용할 헤더 지정
-                .allowCredentials(true);
+                .allowedHeaders("*"); // 허용할 헤더 지정
     }
 }
 
