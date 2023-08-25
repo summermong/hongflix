d863d38e3bf2b8820cd4ef2d1df0b86820ffbc7f
summermong <2514o_o@naver.com>
2023-08-25 11:52:28 +0900

    feat: 프론트 합침

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,11 @@ import Header from './home/Header'; // Header 컴포넌트 import
 // 각 페이지에 대한 컴포넌트 import
 import Home from './home/Home';
 import Category from './pages/Category';
-import Login from './pages/Login';
 import MyPage from './pages/MyPage';
 import Search from './pages/Search';
 import Footer from './home/Footer';
-import Login from "./pages/Login";
-import Join from "./pages/Join";
+import Login from './pages/Login';
+import Join from './pages/Join';
 
 function App() {
   const isLoggedIn = false; // 로그인 여부 상태
--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 
 const MyPage = () => {
   const [subscribed, setSubscribed] = useState(false);
