471e6096a692d4403c4164148d01e8de11d347e5
summermong <2514o_o@naver.com>
2023-08-25 18:16:28 +0900

    feat: 프론트엔드 합병 후 작업

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import axios from "axios";
-import Header from "./home/Header"; // Header 컴포넌트 import
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Header from './home/Header';
+
+// Header 컴포넌트 import
 
 // 각 페이지에 대한 컴포넌트 import
-import Home from "./home/Home";
-import Category from "./pages/Category";
-import MyPage from "./pages/MyPage";
-import Search from "./pages/Search";
-import Footer from "./home/Footer";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import LoginForAdmin from "./pages/LoginForAdmin";
-import SignUpForAdmin from "./pages/SignUpForAdmin";
-import AdminContents from "./admin/AdminContents";
-import AdminHome from "./admin/AdminHome";
-import AdminMembers from "./admin/AdminMembers";
-import AdminMovies from "./admin/AdminMovies";
-import AdminSetting from "./admin/AdminSetting";
+import Home from './home/Home';
+import Category from './pages/Category';
+import MyPage from './pages/MyPage';
+import Search from './pages/Search';
+import Footer from './home/Footer';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import LoginForAdmin from './pages/LoginForAdmin';
+import SignUpForAdmin from './pages/SignUpForAdmin';
+import AdminContents from './admin/AdminContents';
+import AdminHome from './admin/AdminHome';
+import AdminMembers from './admin/AdminMembers';
+import AdminMovies from './admin/AdminMovies';
+import AdminSetting from './admin/AdminSetting';
 
 function App() {
-  const isLoggedIn = false; // 로그인 여부 상태
-  const url = `http://localhost:8080/`;
+  const url = `https://kwyrmjf86a.execute-api.ap-northeast-2.amazonaws.com/`;
+
+  const [isLogined, setIsLogined] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+  console.log(isLogined);
+  console.log(userInfo);
   const inputValue = (e, setValue) => {
     e.preventDefault();
     setValue(e.target.value);
@@ -31,25 +37,26 @@ function App() {
   };
   const inputClear = (e, setValue) => {
     e.preventDefault();
-    setValue("");
+    setValue('');
   };
   const isSignUp = async (userInfo, url) => {
     console.log(userInfo);
-    console.log("email : ", userInfo["email"]);
-    console.log("password : ", userInfo["password"]);
-    console.log("nickName : ", userInfo["nickName"]);
-    console.log(
-      `phoneNumber :  +82${userInfo["phoneNumber"].replaceAll("-", "")}`
-    );
+    console.log('email : ', userInfo['email']);
+    console.log('password : ', userInfo['password']);
+    console.log('nickName : ', userInfo['nickName']);
+    console.log('phoneNumber', userInfo['phoneNumber'].replaceAll('-', ''));
     await axios
       .post(url, {
-        email: userInfo["email"],
-        password: userInfo["password"],
-        nickName: userInfo["nickName"],
-        phoenNumber: userInfo["phoneNumber"],
+        email: userInfo['email'],
+        password: userInfo['password'],
+        nickName: userInfo['nickName'],
+        phoneNumber: userInfo['phoneNumber'].replaceAll('-', ''),
       })
       .then((res) => {
         console.log(res.data);
+        alert(res.data.data);
+        if (res.data.status === 200) {
+        }
       })
       .catch((err) => {
         console.log(err);
@@ -57,9 +64,16 @@ function App() {
   };
   const isLogin = async (loginInfo, url) => {
     await axios
-      .post(url, { email: loginInfo["email"], password: loginInfo["password"] })
+      .post(url, { email: loginInfo['email'], password: loginInfo['password'] })
       .then((res) => {
         console.log(res.data);
+        if (res.data.status === 200) {
+          setIsLogined(true);
+          setUserInfo(res.data.data);
+          alert('로그인이 성공했습니다');
+        } else {
+          alert(res.data.data);
+        }
       })
       .catch((err) => {
         console.log(err);
@@ -72,7 +86,7 @@ function App() {
       <BrowserRouter>
         <div className="flex flex-col h-screen">
           <div className="flex-1">
-            <Header isLoggedIn={isLoggedIn} />
+            <Header isLoggedIn={isLogined} />
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/category" element={<Category />} />
